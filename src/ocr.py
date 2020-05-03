d4276d2ea8fc3f430e4bd9a2043b634f562902de
Lawrence Nguyen <lawrence.nguyen@ucalgary.ca>
2020-05-03 17:52:49 -0600

    Crop to smallest bounding box, partially working

--- src/ocr.py
+++ src/ocr.py
@@ -1,6 +1,8 @@
 import pytesseract
 import cv2
 import sys
+import time
+import numpy as np
 from PIL import Image
 from desktopmagic.screengrab_win32 import (getDisplayRects, getRectAsImage)
 
@@ -8,23 +10,67 @@ def main(display_number):
     # Get the rect of the display
     _display_rect = getDisplayRects()[display_number - 1]
 
-    # Take and crop screenshot to the third quadrant
-    im = getRectAsImage(_display_rect)
-    width, height = im.size
+    _im = getRectAsImage(_display_rect)
+    _width, _height = _im.size
 
-    _crop_left = 0
-    _crop_top = int((height / 3) * 2)
-    _crop_right = int((width / 5))
-    _crop_bottom = height
+    RESIZE_FACTOR = 2
+    IMAGE_CONVERSION_MODE = "LA"
 
-    cropped_im = im.crop((_crop_left, _crop_top, _crop_right, _crop_bottom))
-    #cropped_im = cropped_im.convert("LA")
-    cropped_im.show()
+    # P1
+    _p1_crop_left = int(_width * 0.034)
+    _p1_crop_top = int(_height * 0.95)
+    _p1_crop_right = int(_width * 0.08)
+    _p1_crop_bottom = int(_height * 0.97)
+
+    _p1_cropped_im = _im.crop((_p1_crop_left, _p1_crop_top, _p1_crop_right, _p1_crop_bottom))
+    _p1_cropped_im = _p1_cropped_im.convert(IMAGE_CONVERSION_MODE)
+    _larger_size = tuple(RESIZE_FACTOR * x for x in _p1_cropped_im.size)
+    _p1_cropped_im = _p1_cropped_im.resize(_larger_size, Image.ANTIALIAS)
+    #_p1_cropped_im.show()
+
+    #P2
+    _p2_crop_left = int(_width * 0.035)
+    _p2_crop_top = int(_height * 0.88)
+    _p2_crop_right = int(_width * 0.08)
+    _p2_crop_bottom = int(_height * 0.9)
+
+    _p2_cropped_im = _im.crop((_p2_crop_left, _p2_crop_top, _p2_crop_right, _p2_crop_bottom))
+    _p2_cropped_im = _p2_cropped_im.convert(IMAGE_CONVERSION_MODE)
+    _larger_size = tuple(RESIZE_FACTOR * x for x in _p2_cropped_im.size)
+    _p2_cropped_im = _p2_cropped_im.resize(_larger_size, Image.ANTIALIAS)
+    # _p2_cropped_im.show()
+
+    #P3
+    _p3_crop_left = int(_width * 0.035)
+    _p3_crop_top = int(_height * 0.818)
+    _p3_crop_right = int(_width * 0.08)
+    _p3_crop_bottom = int(_height * 0.84)
+
+    _p3_cropped_im = _im.crop((_p3_crop_left, _p3_crop_top, _p3_crop_right, _p3_crop_bottom))
+    _p3_cropped_im = _p3_cropped_im.convert(IMAGE_CONVERSION_MODE)
+    _larger_size = tuple(RESIZE_FACTOR * x for x in _p3_cropped_im.size)
+    _p3_cropped_im = _p3_cropped_im.resize(_larger_size, Image.ANTIALIAS)
+    # _p3_cropped_im.show()
+
+    #P4
+    _p4_crop_left = int(_width * 0.035)
+    _p4_crop_top = int(_height * 0.818)
+    _p4_crop_right = int(_width * 0.08)
+    _p4_crop_bottom = int(_height * 0.84)
+
+    _p4_cropped_im = _im.crop((_p4_crop_left, _p4_crop_top, _p4_crop_right, _p4_crop_bottom))
+    _p4_cropped_im = _p4_cropped_im.convert(IMAGE_CONVERSION_MODE)
+    _larger_size = tuple(RESIZE_FACTOR * x for x in _p4_cropped_im.size)
+    _p4_cropped_im = _p4_cropped_im.resize(_larger_size, Image.ANTIALIAS)
+    #_p4_cropped_im.show()
+
 
     # OCR
     pytesseract.pytesseract.tesseract_cmd = r"C:\\Users\\Lawrence\\AppData\\Local\\Tesseract-OCR\\tesseract.exe"
-    print(pytesseract.image_to_string(cropped_im))
-
+    print(pytesseract.image_to_string(_p1_cropped_im))
+    print(pytesseract.image_to_string(_p2_cropped_im))
+    print(pytesseract.image_to_string(_p3_cropped_im))
+    #print(pytesseract.image_to_string(_p4_cropped_im))
 
 
 if __name__ == "__main__": 
