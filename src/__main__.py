c494cd4a4a0b2320729660e33faa8bf4e65f74e3
Lawrence Nguyen <lawrence.nguyen@ucalgary.ca>
2020-05-14 23:06:45 -0600

    Make implementation async-friendly

--- src/__main__.py
+++ src/__main__.py
@@ -15,16 +15,5 @@ if __name__ == "__main__":
     menu.show_menu()
     mode = menu.get_choice()
     processor = Processor(display_number, mode)
-    bot_client = DiscordBot()
+    bot_client = DiscordBot(processor, mode)
     bot_client.run(config.DISCORD_BOT_TOKEN)
-    
-    # TODO: Add game-has-started detection, for now just wait 10 seconds until executing
-    time.sleep(10)
-
-    i = 0
-    while (1):
-        i += 1
-        cash_total = processor.get_cash_total(i)
-        if (mode == "4"):
-            input("Press Enter to continue...")
-        time.sleep(10)
--- src/discordBot.py
+++ src/discordBot.py
@@ -1,6 +1,27 @@
+import asyncio
+import time
 import discord
 
 class DiscordBot(discord.Client):
 
+    def __init__(self, processor, mode):
+        super().__init__()
+        self.processor = processor
+        self.mode = mode
+
     async def on_ready(self):
-        print("Discord bot is ready")
+        print("\nDiscord bot is ready\n")
+
+        # TODO: Add game-has-started detection, for now just wait 10 seconds until executing
+
+        i = 0
+        while (1):
+            await asyncio.sleep(10)
+            i += 1
+            cash_total = self.processor.get_cash_total(i)
+
+            if (self.mode == "4"):
+                input("Press Enter to continue...")
+            
+
+    
