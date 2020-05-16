06b63d2014e9a805a0be0a3d33c4f7e32e3007f2
Lawrence Nguyen <lawrence.nguyen@ucalgary.ca>
2020-05-15 22:53:33 -0600

    working implementation of discord message sending with tts

--- src/discordBot.py
+++ src/discordBot.py
@@ -13,12 +13,21 @@ class DiscordBot(discord.Client):
         print("\nDiscord bot is ready\n")
 
         # TODO: Add game-has-started detection, for now just wait 10 seconds until executing
+        await self._main_loop()
 
+    async def _main_loop(self):
+        main_channel = None
+        channels = self.get_all_channels()
+        for channel in channels:
+            if (channel.name == "general"):
+                main_channel = channel
+        
         i = 0
         while (1):
             await asyncio.sleep(10)
             i += 1
             cash_total = self.processor.get_cash_total(i)
+            await main_channel.send("Total cash is {}".format(cash_total), tts = True)
 
             if (self.mode == "4"):
                 input("Press Enter to continue...")
