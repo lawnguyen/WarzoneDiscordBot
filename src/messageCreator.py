2a7013afe120e2669532c80d71443210498beb22
Lawrence Nguyen <lawrence.nguyen@ucalgary.ca>
2020-05-16 03:49:35 -0600

    tweaked some logic in the messaging

--- src/messageCreator.py
+++ src/messageCreator.py
@@ -4,7 +4,9 @@ import math
 __all__ = ["create"]
 
 def create(cash_total, buy_back_count):
-    if (cash_total == 0):
+    if (cash_total < 4000):
+        # For now, don't send a message unless we have >= $4000 since any
+        # lower of a total is not very useful to the entire team
         return None
     
     message = "Total cash is ${}".format(cash_total)
@@ -16,6 +18,7 @@ def create(cash_total, buy_back_count):
 
     if (buy_back_count >= 1 and cash_total >= constants.BUY_BACK_COST):
         buy_back_amount = math.floor(cash_total / constants.BUY_BACK_COST)
-        message += ", you can buy back {} of your teammates".format(buy_back_amount)
+        message += ", you can buy back {} of your teammates".format(
+            min(buy_back_amount, buy_back_count))
 
     return message
